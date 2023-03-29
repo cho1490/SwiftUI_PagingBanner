304b73966e215985b8c8b608c9a39ef4ce7e8fee
조상현 <chosanghyun99@gmail.com>
2023-03-29 10:34:29 +0900

    Initial Commit

---
+++ SwiftUI_BannerView/SwiftUI_BannerViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BannerViewApp.swift
+//  SwiftUI_BannerView
+//
+//  Created by 조상현 on 2023/03/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BannerViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
